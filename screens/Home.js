e12f919741c0c8b8ba6b3c2a9ffac394545a8ffe
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-03-23 14:42:10 +0200

    implement TextInput component

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,32 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { SafeAreaView, Image } from "react-native";
+import { SafeAreaView, Image,ScrollView,TextInput  } from "react-native";
+import { EvilIcons } from '@expo/vector-icons';
+
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <View style={{flexDirection:'row', justifyContent:'space-between', padding:20}}>
-        <Text style={{fontSize:17}}>Hello Noella Promise</Text>
-        <Image source={require("../assets/images/user-profile.jpg")} style={{width:50,height:50,borderRadius:50}} />
-      </View>
+      <ScrollView>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: 20,
+          }}
+        >
+          <Text style={{ fontSize: 14 }}>Hello Noella Promise</Text>
+          <Image
+            source={require("../assets/images/user-profile.jpg")}
+            style={{ width: 50, height: 50, borderRadius: 50 }}
+          />
+        </View>
+<View style={{flexDirection:"row", paddingHorizontal:10, paddingVertical:8, borderColor:'#C6C6C6',borderWidth:2,borderRadius:8,marginHorizontal:20}}>
+<EvilIcons name="search" size={25} color="#c6c6c6" style={{marginTop:5}}/>
+<TextInput placeholder="Search" />
+</View>
+        
+      </ScrollView>
     </SafeAreaView>
   );
 };
