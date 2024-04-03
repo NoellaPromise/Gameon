aaa26f94bc71a5a21a1eaa8040830631b6f04b7e
Noella Claire DUSHAKIMANA <dushakenoella@gmail.com>
2024-04-03 17:58:55 +0200

    add text and play button

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,24 @@
 import { View, Text,Image } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const ListItem = () => {
   return (
-    <View>
+    <View style={{flexDirection:'row', justifyContent:'space-between',alignItems:'center',marginBottom:20}}>
       <View style={{flexDirection:'row',alignItems:'center',flex:1}}>
       <Image source={require('../assets/images/FarCry6 (1).png')} 
       style={{width:55,height:55,borderRadius:10, marginLeft:30, marginRight:8}}
       
       />
+      <View>
+        <Text>Marvel</Text>
+        <Text>SPIDER-MAN</Text>
+      </View>
         </View>
+        <TouchableOpacity
+        style={{backgroundColor:'#0aada8', padding:18,width:100,borderRadius:10}}>
+            <Text style={{color:'#fff', textAlign:'center',fontSize:14}}>Play</Text>
+        </TouchableOpacity>
     </View>
   )
 }
